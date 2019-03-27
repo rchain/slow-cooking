24293b6b9e0481d2ac00d14893807496b7e44039
Adam Szkoda <adaszko@gmail.com>
2019-03-27 14:24:13 +0100

    Start slow cooking jobs on push to dev

--- rhobot/rhobot.py
+++ rhobot/rhobot.py
@@ -6,6 +6,7 @@ import importlib
 import traceback
 import subprocess
 
+from loguru import logger
 import aiohttp
 from aiohttp import web
 import cachetools
@@ -14,50 +15,59 @@ from gidgethub import routing
 from gidgethub import sansio
 
 
-router = routing.Router()
-cache = cachetools.LRUCache(maxsize=500)
-
-
 def get_last_build_number():
-    command = 'drone build ls --format {{.Number}} --limit 1 rchain/rchain'
-    return subprocess.check_output(command)
+    drone_cmd = os.environ['DRONE_CMD']
+    command = [drone_cmd, 'build', 'ls', '--format={{.Number}}', '--limit=1', 'rchain/slow-cooking']
+    output = subprocess.check_output(command)
+    return int(output.decode().strip())
 
 
-def start_drone_build(repo, proto_build_number):
-    command = 'drone build restart {} {}'.format(repo, proto_build_number)
-    return subprocess.check_output(command)
+def start_drone_build(proto_build_number):
+    drone_cmd = os.environ['DRONE_CMD']
+    command = [drone_cmd, 'build', 'restart', 'rchain/slow-cooking', str(proto_build_number)]
+    output = subprocess.check_output(command)
+    logger.info(output.decode().strip())
 
 
-@router.register('push', ref='refs/heads/dev')
-def pushed_to_dev(event, gh, *arg, **kwargs):
-    print(event)
+async def pushed_to_dev(event):
+    last_build_number = get_last_build_number()
+    start_drone_build(last_build_number)
 
 
-async def handle_request(request):
-    try:
-        body = await request.read()
-        secret = os.environ.get("GITHUB_WEBHOOK_SECRET")
-        event = sansio.Event.from_http(request.headers, body, secret=secret)
-        print('GH delivery ID', event.delivery_id, file=sys.stderr)
-        if event.event == "ping":
-            return web.Response(status=200)
-        oauth_token = os.environ.get("GITHUB_PERSONAL_TOKEN")
-        async with aiohttp.ClientSession() as session:
-            gh = gh_aiohttp.GitHubAPI(session, "rchain/rchain", oauth_token=oauth_token, cache=cache)
-            # Give GitHub some time to reach internal consistency.
-            await asyncio.sleep(1)
-            await router.dispatch(event, gh)
-        try:
-            print('GH requests remaining:', gh.rate_limit.remaining)
-        except AttributeError:
-            pass
+async def handle_request(request, secret, oauth_token):
+    body = await request.read()
+
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    logger.info('Got event {}', event.delivery_id)
+    if event.event == 'ping':
         return web.Response(status=200)
+
+    async with aiohttp.ClientSession() as session:
+        if event.event == 'push' and event.data['ref'] == 'refs/heads/dev':
+            await pushed_to_dev(event)
+
+    return web.Response(status=200)
+
+
+async def try_handle_request(request):
+    github_webhook_secret = os.environ['GITHUB_WEBHOOK_SECRET']
+    github_personal_token = os.environ['GITHUB_PERSONAL_TOKEN']
+    try:
+        await handle_request(request, github_webhook_secret, github_personal_token)
     except Exception as exc:
         traceback.print_exc(file=sys.stderr)
         return web.Response(status=500)
 
+    return web.Response(status=200)
+
+
+async def handle_health(request):
+    return web.Response(status=200)
+
 
 if __name__ == "__main__":
     app = web.Application()
-    app.router.add_post("/", handle_request)
+    app.router.add_get("/health", handle_health)
+    app.router.add_post("/", try_handle_request)
     web.run_app(app, port=9090)
