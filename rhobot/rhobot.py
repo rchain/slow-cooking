ef7a271e776b1d94aacd0370f7b3f04fa7f976e1
Adam Szkoda <adaszko@gmail.com>
2019-03-28 09:25:49 +0100

    More informative logs

--- rhobot/rhobot.py
+++ rhobot/rhobot.py
@@ -22,16 +22,16 @@ def get_last_build_number():
     return int(output.decode().strip())
 
 
-def start_drone_build(proto_build_number):
+def start_drone_build(logger_context, proto_build_number):
     drone_cmd = os.environ['DRONE_CMD']
     command = [drone_cmd, 'build', 'restart', 'rchain/slow-cooking', str(proto_build_number)]
     output = subprocess.check_output(command)
-    logger.info(output.decode().strip())
+    logger_context.info(output.decode().strip())
 
 
-async def pushed_to_dev(event):
+async def pushed_to_dev(logger_context, event):
     last_build_number = get_last_build_number()
-    start_drone_build(last_build_number)
+    start_drone_build(logger_context, last_build_number)
 
 
 async def handle_request(request, secret, oauth_token):
@@ -39,13 +39,14 @@ async def handle_request(request, secret, oauth_token):
 
     event = sansio.Event.from_http(request.headers, body, secret=secret)
 
-    logger.info('Got event {}', event.delivery_id)
+    logger_context = logger.bind(delivery_id=event.delivery_id, event=event.event, ref=event.data['ref'])
+    logger_context.info('Got event')
     if event.event == 'ping':
         return web.Response(status=200)
 
     async with aiohttp.ClientSession() as session:
         if event.event == 'push' and event.data['ref'] == 'refs/heads/dev':
-            await pushed_to_dev(event)
+            await pushed_to_dev(logger_context, event)
 
     return web.Response(status=200)
 
@@ -53,10 +54,11 @@ async def handle_request(request, secret, oauth_token):
 async def try_handle_request(request):
     github_webhook_secret = os.environ['GITHUB_WEBHOOK_SECRET']
     github_personal_token = os.environ['GITHUB_PERSONAL_TOKEN']
+
     try:
         await handle_request(request, github_webhook_secret, github_personal_token)
     except Exception as exc:
-        traceback.print_exc(file=sys.stderr)
+        logger.exception('Exception while handling request')
         return web.Response(status=500)
 
     return web.Response(status=200)
